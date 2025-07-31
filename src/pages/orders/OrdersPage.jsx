7d4529c575cf824a79f1ae84e0506167ce81fe93
Marielle <marrielle.aboujaoude@montymobile.com>
2025-07-31 10:52:59 +0300

    Update OrdersPage.jsx

--- src/pages/orders/OrdersPage.jsx
+++ src/pages/orders/OrdersPage.jsx
@@ -205,6 +205,7 @@ function OrdersPage() {
                 end={el?.amount / 100.0} //because the amount is from stripe by cent
                 duration={1.5}
                 separator=","
+                decimals={2}
               />
             </TableCell>
             <TableCell
